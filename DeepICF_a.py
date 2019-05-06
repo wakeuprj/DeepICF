7ac72187b36472bf358f5456ce290ea6b06bece6
Rishabh Jain <wakeuprj@gmail.com>
2019-05-06 13:17:46 +0100

    try different loss when training deepICF_a include gama bilinear beta bilinear etc.

--- DeepICF_a.py
+++ DeepICF_a.py
@@ -206,10 +206,10 @@ class DeepICF_a:
 
     def _create_loss(self):
         with tf.name_scope("loss"):
-            self.loss = tf.losses.log_loss(self.labels, self.output)
-                        # self.lambda_bilinear * tf.reduce_sum(tf.square(self.embedding_Q)) + \ , reduction=tf.losses.Reduction.MEAN
-                        # self.gamma_bilinear * tf.reduce_sum(tf.square(self.embedding_Q_)) + \
-                        # self.eta_bilinear * tf.reduce_sum(tf.square(self.W))
+            self.loss = tf.losses.log_loss(self.labels, self.output) + \
+                        self.lambda_bilinear * tf.reduce_sum(tf.square(self.embedding_Q)) + \
+                        self.gamma_bilinear * tf.reduce_sum(tf.square(self.embedding_Q_)) + \
+                        self.eta_bilinear * tf.reduce_sum(tf.square(self.W))
 
             for i in range(min(len(self.n_hidden), len(self.reg_W))):
                 if self.reg_W[i] > 0:

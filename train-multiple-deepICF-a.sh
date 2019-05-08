bbf9debb274445c7c7b2184db7bc6928dfb22b19
Rishabh Jain <wakeuprj@gmail.com>
2019-05-08 22:58:48 +0100

    20 epochs.

--- train-multiple-deepICF-a.sh
+++ train-multiple-deepICF-a.sh
@@ -5,6 +5,6 @@ if [ ! -d "Stability-Models-DeepICF-a" ]; then
 fi
 for i in $(seq 0 $1)
 do
-  python3 DeepICF_a.py --path Data/ --dataset ml-1m --epochs 15 --beta 0.8 --weight_size 16 --activation 0 --algorithm 0 --verbose 1 --batch_choice user --embed_size 16 --layers [64,32,16] --regs [1e-06,1e-06,1e-06] --reg_W [10,10,10,10] --alpha 0 --train_loss 1 --num_neg 4 --lr 0.01 --batch_norm 1 --pretrain 0
+  python3 DeepICF_a.py --path Data/ --dataset ml-1m --epochs 20 --beta 0.8 --weight_size 16 --activation 0 --algorithm 0 --verbose 1 --batch_choice user --embed_size 16 --layers [64,32,16] --regs [1e-06,1e-06,1e-06] --reg_W [10,10,10,10] --alpha 0 --train_loss 1 --num_neg 4 --lr 0.01 --batch_norm 1 --pretrain 0
 done
 
